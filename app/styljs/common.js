f440fa1fa5da6cf01c864ae2f90c679bb29667c2
Artemy <art_tensai@mail.ru>
2018-09-19 12:16:44 +0500

    update

--- app/styljs/common.js
+++ app/styljs/common.js
@@ -1,9 +1,9 @@
 ;(function ($) {
 
     $(function() {
-
         svg4everybody();
         $('[target="_blank"]').attr('rel','noopener noreferrer');
+        $('form').addClass('-visor-no-click');
         
     });
 	
