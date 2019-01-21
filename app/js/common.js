15891310a84a08bba809776a706b137d9db39c55
milkamil93 <milkamil93@gmail.com>
2019-01-21 13:19:35 +0500

    update

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,15 @@
 ;(function ($) {
 
+    svg4everybody();
+    $('[target="_blank"]').attr('rel','noopener noreferrer');
+    $('form').addClass('-visor-no-click');
+
     $(function() {
-        svg4everybody();
-        $('[target="_blank"]').attr('rel','noopener noreferrer');
-        $('form').addClass('-visor-no-click');
-        
+        $('[type="tel"]').inputmask('+7 (999) 999-99-99', {
+            'onincomplete': function () {
+                $(this).val('');
+            }
+        });
     });
 	
 })(jQuery);
