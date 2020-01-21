3e1fb9b49d6fb638deb083e49f248db9b94aed93
milkamil93 <milkamil93@gmail.com>
2020-01-21 11:15:51 +0500

    update

--- gulpfile.js
+++ gulpfile.js
@@ -158,8 +158,14 @@ function jsCommon() {
             module: {
                 rules: [
                     {
-                        test: /\.(js)$/,
-                        loader: 'babel-loader'
+                        test: /\.js$/,
+                        exclude: /\.node_modules$/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['@babel/preset-env']
+                            }
+                        }
                     }
                 ]
             },
